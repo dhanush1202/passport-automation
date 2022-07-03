ca69d9b82e5c59ff92891fa0fb2f2e9e0d933bbd
dhanush1202 <dhanushg25@gmail.com>
2022-07-03 20:38:44 +0530

    login

--- app/src/main/java/com/example/passportautomation/MainActivity.java
+++ app/src/main/java/com/example/passportautomation/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.passportautomation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+Button applicant;
+Button ppad;
+Button ream;
+Button pol;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        applicant=findViewById(R.id.applicant);
+        ppad=findViewById(R.id.ppad);
+        ream=findViewById(R.id.ream);
+        pol=findViewById(R.id.pol);
+
+        applicant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(MainActivity.this,applicant.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/passportautomation/applicant.java
+++ app/src/main/java/com/example/passportautomation/applicant.java
@@ -4,15 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class applicant extends AppCompatActivity {
+public class applicant extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 Button login;
 EditText id;
 EditText pass;
 TextView sign;
+Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +27,32 @@ TextView sign;
         id=findViewById(R.id.id);
         pass=findViewById(R.id.pass);
         sign=findViewById(R.id.signin);
-    login.setOnClickListener(new View.OnClickListener() {
+        spinner=findViewById(R.id.spinner);
+ArrayAdapter<CharSequence> ad=ArrayAdapter.createFromResource(this,R.array.person, android.R.layout.simple_spinner_item);
+ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+spinner.setAdapter(ad);
+spinner.setOnItemSelectedListener(this);
+login.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             String idno=id.getText().toString();
+            System.out.println(idno);
             String passw=pass.getText().toString();
+            System.out.println(passw);
+            Toast.makeText(applicant.this, "sucessfully logged in ", Toast.LENGTH_SHORT).show();
 
         }
     });
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String choice=adapterView.getItemAtPosition(i).toString();
+        Toast.makeText(this, choice, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
 }
